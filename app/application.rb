55e1ef9c15b84e7b71bbca3de43cc8c083f3e770
Michael Carroll <carrollm2@gmail.com>
2020-01-24 02:06:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item_name = req.path.split("/items/").last
       item_found = @@items.find{|item| item.name == item_name}
 
-      resp.write item_found.price
+      resp.write item_found.price.to_s
       resp.status = 200
     else
       resp.write "Route not found"
