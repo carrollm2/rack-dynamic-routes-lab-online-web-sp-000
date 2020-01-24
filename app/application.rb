19e17eb1a954ce1d230b195d02dda3b7bb96a4c8
Michael Carroll <carrollm2@gmail.com>
2020-01-24 01:55:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path=="/items"
+    if req.path.match(/items/)
       binding.pry
       resp.write "#{}"
       resp.status = 200
