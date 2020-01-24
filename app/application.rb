6c2781fc19ffa2eea4d44237b84bc4f9a6a3e3c9
Michael Carroll <carrollm2@gmail.com>
2020-01-24 02:01:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      binding.pry
+
       item_name = req.path.split("/items/").last
       item_found = @@items.find{|item| item.name == item_name}
 
